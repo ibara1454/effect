d3b7036bad146347c6650ff7c27f004d0d547d97
Ibara <ibara1454@gmail.com>
2019-12-23 00:17:05 +0900

    docs: updates function docs

--- src/main/kotlin/com/github/ibara1454/effect/Effect.kt
+++ src/main/kotlin/com/github/ibara1454/effect/Effect.kt
@@ -26,54 +26,83 @@ package com.github.ibara1454.effect
 
 /**
  * [effect] allows you to make side-effects [action] with the result of given function [f].
+ * If any exception is thrown by [f], then it will propagate immediately outside of [effect]
+ * and the side-effect [action] will not be triggered.
  *
- * In general, [action] should not contain any business logic and should not throw exceptions.
- *
- * @param action any side-effect, e.g. loggers and builders
+ * @param action any side-effect, e.g. loggers and builders.
+ *  In general, [action] should not contain any business logic and should not throw exceptions
  * @param f the function you actually want to call
- * @return which is same to the result of [f]
+ * @return same as the return value of [f]
  */
 inline fun <T> effect(action: (T) -> Unit, f: () -> T): T =
     runCatching(f).onSuccess(action).getOrThrow()
 
 /**
- * TBD.
- * @param action TBD
- * @param f TBD
- * @return TBD
+ * Make side-effects [action] when the given function [f] throws exceptions.
+ * The side-effect function [action] will only triggered if any exception is thrown by [f].
+ * After that, the exception will propagate outside of [effectOnError].
+ *
+ * @param action any side-effect, e.g. loggers and builders.
+ *  In general, [action] should not contain any business logic and should not throw exceptions
+ * @param f the function you actually want to call
+ * @return same as the return value of [f]
  */
 inline fun <T> effectOnError(action: (Throwable) -> Unit, f: () -> T): T =
     runCatching(f).onFailure(action).getOrThrow()
 
 /**
- * TBD.
- * @param action TBD
- * @param f TBD
- * @return TBD
+ * Make side-effects [action] after each invocation of given function [f].
+ *
+ * @param action any side-effect, e.g. loggers and builders.
+ *  In general, [action] should not contain any business logic and should not throw exceptions
+ * @param f the function you actually want to call
+ * @return same as the return value of [f]
  */
+@SinceKotlin("1.3")
 inline fun <T> effectOnResult(action: (Result<T>) -> Unit, f: () -> T): T =
     runCatching(f).also(action).getOrThrow()
 
 /**
- * TBD.
- * @param action TBD.
- * @return higher-order function
+ * Receive side-effect function [action] to build a high-order function which receives a function `f`
+ * that we want to call actually. If any exception is thrown by `f`, then it will propagate immediately
+ * to the caller and the side-effect [action] will not be triggered.
+ * Note that this function is just returned the partial application on [action] of the two-arity
+ * function [effect].
+ *
+ * @see [effect]
+ * @param action any side-effect, e.g. loggers and builders
+ * @return a higher-order function receives a function `f` and makes side-effect by using the return
+ *  value of `f`
  */
 inline fun <T> buildEffect(crossinline action: (T) -> Unit): (() -> T) -> T =
     { f -> effect(action, f) }
 
 /**
- * TBD.
- * @param action TBD
- * @return TBD
+ * Receive side-effect function [action] to build a high-order function which receives a function `f`
+ * that we want to call actually. The side-effect function [action] will only triggered if any exception
+ * is thrown by `f`. After that, the exception will propagate immediately to the caller.
+ * Note that this function is just returned the partial application on [action] of the two-arity
+ * function [effectOnError].
+ *
+ * @param action any side-effect, e.g. loggers and builders
+ * @return a higher-order function receives a function `f` and makes side-effect if any exception is
+ *  thrown by `f`
  */
 inline fun <T> buildEffectOnError(crossinline action: (Throwable) -> Unit): (() -> T) -> T =
     { f -> effectOnError(action, f) }
 
 /**
- * TBD.
- * @param action TBD
- * @return TBD
+ * Receive side-effect function [action] to build a high-order function which receives a function `f`
+ * that we want to call actually. The side-effect function [action] will triggered by either any exception
+ * is thrown by `f` or the return value of `f`. If any exception is thrown by `f`, if will first trigger the
+ * side-effect function [action], and after that, it will propagate to the caller.
+ * Note that this function is just returned the partial application on [action] of the two-arity
+ * function [effectOnResult].
+ *
+ * @param action any side-effect, e.g. loggers and builders
+ * @return a higher-order function receives a function `f` and makes side-effect either any exception is
+ *  thrown by `f` or `f` finished with success.
  */
+@SinceKotlin("1.3")
 inline fun <T> buildEffectOnResult(crossinline action: (Result<T>) -> Unit): (() -> T) -> T =
     { f -> effectOnResult(action, f) }
